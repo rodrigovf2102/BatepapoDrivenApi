ef8609e66ca08afbbf53d08364fdc9a68ad2fa35
Rodrigo <rodrigovf2102@gmail.com>
2022-09-01 12:09:34 -0300

    using dotenv and get/participants returning all users

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
 
 const server = express();
 server.use(cors());
 server.use(express.json());
+dotenv.config();
+
+const mongoClient  = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(()=> {db = mongoClient.db('uol-backend');});
+
+server.get('/participants', async (req,res)=>{
+    try{
+        const users = await db.collection('users').find().toArray();
+        res.send(users);
+    }catch{
+        res.status(500).send("Server error");
+    }
+})
+
 
 
 
